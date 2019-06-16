08aebd49b04ffb67e2b545edbf34a06d45c5a6c3
Justin Canode <jcanode@my.gcu.edu>
2019-06-15 19:47:21 -0700

    Added about page

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from '../components/head'
+import Nav from '../components/nav'
+
+const About = () => (<div></div>)
+
+export default About
