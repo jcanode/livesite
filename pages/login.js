0e14fbba534ce2566e3825d4910328f3206836bc
jcanode <45806280+jcanode@users.noreply.github.com>
2019-09-26 23:02:50 -0700

    updated login page

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Input from '@material-ui/core/Input';
+import TextField from '@material-ui/core/TextField'
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Link from 'next/link'
 import Head from '../components/head'
 import Nav from '../components/nav'
 
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+const handleChange = name => event => {
+  setValues({ ...values, [name]: event.target.value });
+};
 
 const login = () => (
+  
   
 <div className="landingPage">
 <Head title="Login"/>
@@ -16,16 +31,25 @@ const login = () => (
       </Link>
       </div>
       <h1>Welcome to the login page</h1>
-        <Input 
-        placeholder="username"
+       
+        <TextField 
+          label="Username"
+          margin="normal"
         />
+        <br/>
 
+        
+       
+       <TextField
+        id="outlined-password-input"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        margin="normal"
+        
+      />
         <br/>
-        <Input 
-        placeholder="password"
-        />
-        <br/>
-      <button variant="contained">Submit</button>
+      <button variant="contained" color="primary" >Submit</button>
 
     <style jsx>{`:global(body){background-color: #A9A9A9; margin: 0 0 0 0;} .hero {
         text-align: right;
@@ -40,4 +64,4 @@ const login = () => (
 
 )
 
-export default login  
+export default     login  
